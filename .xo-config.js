140fe2265f8d7ebb99c4288c4a60de5a89ed3267
8845musign <hiroki.yokouchi@dr-ubie.com>
2024-02-12 22:29:35 +0900

    apply blog example
    Signed-off-by: 8845musign <hiroki.yokouchi@dr-ubie.com>

--- .xo-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  prettier: true,
-  "rules": {
-    "unicorn/prevent-abbreviations": "off",
-  }
-}
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,16 @@
-/// <reference types="vitest" />
-import {defineConfig} from 'vitest/config';
+import pages from "@hono/vite-cloudflare-pages";
+import honox from "honox/vite";
+import client from "honox/vite/client";
+import { defineConfig } from "vite";
 
-export default defineConfig({
-	test: {
-		globals: true,
-	},
+export default defineConfig(({ mode }) => {
+  if (mode === "client") {
+    return {
+      plugins: [client()],
+    };
+  } else {
+    return {
+      plugins: [honox(), pages()],
+    };
+  }
 });
