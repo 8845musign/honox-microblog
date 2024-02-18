7d5b6f4ec1c7fbaad2df18a79b8c6602cf6eb0ac
8845musign <hiroki.yokouchi@dr-ubie.com>
2024-02-18 15:32:04 +0900

    protect post page
    Signed-off-by: 8845musign <hiroki.yokouchi@dr-ubie.com>

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,7 @@
 import { css } from "hono/css";
 import { createRoute } from "honox/factory";
 import Counter from "../islands/counter";
+import { basicAuth } from "hono/basic-auth";
 
 import type { Article } from "../db";
 import { findAllArticles, initilizeDatabase } from "../db";
@@ -13,27 +14,28 @@ const className = css`
   font-style: normal;
 `;
 
-export default createRoute(async (c) => {
-  await initilizeDatabase(c.env.DB);
-  const articles = await findAllArticles(c.env.DB);
+export default createRoute(
+  basicAuth({ username: "test", password: "test" }),
+  async (c) => {
+    await initilizeDatabase(c.env.DB);
+    const articles = await findAllArticles(c.env.DB);
 
-  console.log(articles);
+    const name = c.req.query("name") ?? "Hono";
+    return c.render(
+      <div class={className}>
+        <h1>Hello, {name}!</h1>
+        <Counter />
 
-  const name = c.req.query("name") ?? "Hono";
-  return c.render(
-    <div class={className}>
-      <h1>Hello, {name}!</h1>
-      <Counter />
+        {articles.map((article: Article) => (
+          <div key={article.id}>
+            <p>{article.content}</p>
+            <p>{article.created_at}</p>
+          </div>
+        ))}
 
-      {articles.map((article: Article) => (
-        <div key={article.id}>
-          <p>{article.content}</p>
-          <p>{article.created_at}</p>
-        </div>
-      ))}
-
-      <a href="/create">POST</a>
-    </div>,
-    { title: name }
-  );
-});
+        <a href="/create">POST</a>
+      </div>,
+      { title: name }
+    );
+  }
+);
